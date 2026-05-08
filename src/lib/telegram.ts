a4150e3e27d866fb2971d8056ca75c5ddf67a39e
Afrizal <afrizal@example.com>
2026-05-08 22:42:39 +0700

    feat: add telegram stub to fix build

---
+++ src/lib/telegram.ts
@@ -0,0 +1,8 @@
+export async function sendTelegramMessage(_text: string) {
+  // TODO: implement
+}
+
+export async function checkStockAndNotify() {
+  // TODO: implement
+  return { notified: 0 };
+}
